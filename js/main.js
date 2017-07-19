48652d7e9241d516fb6448549280f49e6c24498d
Ayushi Chauhan <ayushi.chauhan@capitalone.com>
2017-07-19 11:16:22 -0400

    used functions to organize js

--- js/main.js
+++ js/main.js
@@ -3,18 +3,21 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if(cardsInPlay.length==2){
-  if(cardsInPlay[0]===cardsInPlay[1])
-    alert("You found a match!")
-  else {
-    alert("Sorry, try again")
+var checkForMatch = function(){
+  if(cardsInPlay.length===2){
+    if(cardsInPlay[0]===cardsInPlay[1])
+      console.log("You found a match!");
+    else
+      console.log("Sorry, try again");
   }
 }
+
+var cardNum;
+var flipCard = function(cardNum){
+  console.log("User flipped " + cards[cardNum]);
+  cardsInPlay.push(cards[cardNum]);
+
+  checkForMatch();
+}
+flipCard(0);
+flipCard(3);
